af18061b7367daf0d9121657442617233bc6f14f
Luis Cruz <luisgrabielcruz@gmail.com>
2022-04-28 21:42:03 -0400

    ready for tcp

--- main.go
+++ main.go
@@ -2,22 +2,40 @@ package main
 
 import (
 	"fmt"
-	"html"
-	"log"
-	"net/http"
+	"net"
+	"os"
+)
+
+const (
+	CONN_HOST = "localhost"
+	CONN_PORT = "8081"
+	CONN_TYPE = "tcp"
 )
 
 func main() {
+	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+	if err != nil {
+		fmt.Println("Error listening: ", err.Error())
+		os.Exit(1)
+	}
+	defer l.Close()
+	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		go handleRequest(conn)
+	}
+}
 
-	fmt.Println("Running on port 8081")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
-
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
-	})
-
-	log.Fatal(http.ListenAndServe(":8081", nil))
-
+func handleRequest(conn net.Conn) {
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading: ", err.Error())
+	}
+	fmt.Println(buf)
+	conn.Close()
 }
